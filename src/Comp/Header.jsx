75406c6894f22ce1c31454515888615a04edafa0
Tamil3012 <tamilvalavan115@gmail.com>
2024-09-25 13:16:46 +0530

    done

--- src/Comp/Header.jsx
+++ src/Comp/Header.jsx
@@ -14,10 +14,10 @@ export const Header = () => {
                 <div><img className='w-14' src={logo} alt="" /></div>
             </div>
             <div className='sm:mx-10 mx-4 flex justify-around w-32 md:w-20'>
-              <a href="" target='_blank'>
+              <a href="https://www.instagram.com/tamil.va11?igsh=MXRoY3htN2dvcGdtbg==" target='_blank'>
                 <RiInstagramFill className='text-gray-600 hover:text-main-cl' size={25}/>
               </a>
-              <a href="" target='_blank'>
+              <a href="https://github.com/Tamil3012/Kural" target='_blank'>
                 <BsGithub className='text-gray-600 hover:text-main-cl' size={25}/>
               </a>
             </div>
