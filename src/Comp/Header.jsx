b4e2e48ba5d8a431fb6e3a75e3daa3b8c155161a
Tamil3012 <tamilvalavan115@gmail.com>
2024-10-05 07:31:06 +0530

    insta update

--- src/Comp/Header.jsx
+++ src/Comp/Header.jsx
@@ -14,7 +14,7 @@ export const Header = () => {
                 <div><img className='w-14' src={logo} alt="" /></div>
             </div>
             <div className='sm:mx-10 mx-4 flex justify-around w-32 md:w-20'>
-              <a href="https://www.instagram.com/tamil.va11?igsh=MXRoY3htN2dvcGdtbg==" target='_blank'>
+              <a href="https://www.instagram.com/t_valavan_?igsh=MXRoY3htN2dvcGdtbg==" target='_blank'>
                 <RiInstagramFill className='text-gray-600 hover:text-main-cl' size={25}/>
               </a>
               <a href="https://github.com/Tamil3012/Kural" target='_blank'>
